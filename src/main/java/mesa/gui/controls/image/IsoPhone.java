4372bda6549125920b512d54f1d20f2d73f620a0
Zinelabidine Teyar <zinou.teyar@gmail.com>
2021-11-28 07:59:04 +0100

    refactor isoPhone transitions

--- src/main/java/mesa/gui/controls/image/IsoPhone.java
+++ src/main/java/mesa/gui/controls/image/IsoPhone.java
@@ -1,6 +1,6 @@
 package mesa.gui.controls.image;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
@@ -16,174 +16,64 @@ public class IsoPhone extends LayerIcon {
 	private static double duration = .3;
 
 	private Timeline colorNormal;
-	private Timeline colorError;
-	private Timeline colorSms;
-	private Timeline colorCorrect;
-	
-	private Timeline showError;
-	private Timeline hideError;
 
-	private Timeline showSms;
-	private Timeline hideSms;
-
-	private Timeline showCorrect;
-	private Timeline hideCorrect;
-
-	private Timeline showIncorrect;
-	private Timeline hideIncorrect;
-	
+	private ArrayList<Transition> transitions;
 	public IsoPhone(double size) {
 		super(size, "ips", "ipf", "ipe", "ipc", "ipr", "ipm", "ipt", "ipi");
-		
-		setFocusTraversable(true);
-		
-		init(4, size, 1);
-		init(5, size, -1);
-		init(6, size, -1);
-		init(7, size, 1);
 
-		colorError =  new Timeline(new KeyFrame(Duration.seconds(duration),
-				new KeyValue(getFillProperty(3), Colors.Error, Interpolator.EASE_BOTH)));
+		setFocusTraversable(true);
+
+		transitions = new ArrayList<>();
 		
-		colorSms = new Timeline(new KeyFrame(Duration.seconds(duration),
-				new KeyValue(getFillProperty(3), Colors.GREEN, Interpolator.EASE_BOTH)));
-		
-		colorCorrect = new Timeline(new KeyFrame(Duration.seconds(duration),
-				new KeyValue(getFillProperty(3), Colors.GREEN, Interpolator.EASE_BOTH)));
-		
-		showError = showLayer(4);
-		hideError = hideLayer(4, size, 1);
-		
-		showSms = showLayer(5);
-		hideSms = hideLayer(5, size, -1);
-		
-		showCorrect = showLayer(6);
-		hideCorrect = hideLayer(6, size, -1);
-		
-		showIncorrect = showLayer(7);
-		hideIncorrect = hideLayer(7, size, 1);
-	}
-	
-	private void init(int layer, double size, int by) {
-		setTranslateX(layer, by * size / 4.0);
-		setTranslateY(layer, by * size / (4.0 * factor));
-		setOpacity(layer, 0.0);
-	}
-	
-	private HashMap<Integer, Boolean> mutex = new HashMap<>();
-	
-	private Timeline showLayer(int layer) {
-		Timeline res = new Timeline(new KeyFrame(Duration.seconds(duration),
-				new KeyValue(translateXProperty(layer), 0, SplineInterpolator.ANTICIPATEOVERSHOOT),
-				new KeyValue(translateYProperty(layer), 0, SplineInterpolator.ANTICIPATEOVERSHOOT),
-				new KeyValue(opacityProperty(layer), 1, SplineInterpolator.ANTICIPATEOVERSHOOT)
-			));
-		res.setOnFinished(e-> mutex.put(layer, false));
-		return res;
-	}
-	
-	private Timeline hideLayer(int layer, double size, int by) {
-		Timeline res = new Timeline(new KeyFrame(Duration.seconds(duration),
-				new KeyValue(translateXProperty(layer), by * size / 4.0, SplineInterpolator.ANTICIPATEOVERSHOOT),
-				new KeyValue(translateYProperty(layer), by * size / (4.0 * factor), SplineInterpolator.ANTICIPATEOVERSHOOT),
-				new KeyValue(opacityProperty(layer), 0, SplineInterpolator.ANTICIPATEOVERSHOOT)
-			));
-		res.setOnFinished(e-> mutex.put(layer, false));
-		return res;
+		transitions.add(new Transition(4, size, 1));
+		transitions.add(new Transition(5, size, -1));
+		transitions.add(new Transition(6, size, -1));
+		transitions.add(new Transition(7, size, 1));
 	}
 
 	private void stopAll() {
 		colorNormal.stop();
-		colorError.stop();
-		colorCorrect.stop();
+		transitions.forEach(Transition::stop);
+	}
+
+	private void showTransition(int transition) {
+		if(transitions.get(transition).isShowing()) {
+			return;
+		}
 		
-		showError.stop();
-		hideError.stop();
-		showSms.stop();
-		hideSms.stop();
-		showCorrect.stop();
-		hideCorrect.stop();
-		showIncorrect.stop();
-		hideIncorrect.stop();
+		stopAll();
+		hideOthers(transition);
+		transitions.get(transition).playShow();
+	}
+	
+	private void hideOthers(int transition) {
+		for(int i = 0; i < transitions.size(); i++) {
+			if(i != transition) {
+				transitions.get(i).playHide();
+			}
+		}
 	}
 	
 	public void showError() {
-		if(Boolean.TRUE.equals(mutex.get(4))) {
-			return;
-		}
-		mutex.put(4, true);
-		
-		stopAll();
-
-		hideSms.playFromStart();
-		hideCorrect.playFromStart();
-		hideIncorrect.playFromStart();
-		
-		colorError.playFromStart();
-		showError.playFromStart();
+		showTransition(0);
 	}
-	
+
 	public void showSms() {
-		if(Boolean.TRUE.equals(mutex.get(5))) {
-			return;
-		}
-		mutex.put(5, true);
-		
-		stopAll();
-
-		hideError.playFromStart();
-		hideCorrect.playFromStart();
-		hideIncorrect.playFromStart();
-		
-		colorSms.playFromStart();
-		showSms.playFromStart();
+		showTransition(1);
 	}
-	
+
 	public void showCorrect() {
-		if(Boolean.TRUE.equals(mutex.get(6))) {
-			return;
-		}
-		mutex.put(6, true);
-		
-		stopAll();
-
-		hideError.playFromStart();
-		hideSms.playFromStart();
-		hideIncorrect.playFromStart();
-		
-		colorCorrect.playFromStart();
-		showCorrect.playFromStart();
+		showTransition(2);
 	}
-	
-	public void showIncorrect() {
-		if(Boolean.TRUE.equals(mutex.get(7))) {
-			return;
-		}
-		mutex.put(7, true);
-		
-		stopAll();
 
-		hideError.playFromStart();
-		hideSms.playFromStart();
-		hideCorrect.playFromStart();
-		
-		colorError.playFromStart();
-		showIncorrect.playFromStart();
+	public void showIncorrect() {
+		showTransition(3);
 	}
 
 	public void showNormal() {
-		if(Boolean.TRUE.equals(mutex.get(0))) {
-			return;
-		}
-		mutex.put(0, true);
-		
 		stopAll();
-
-		hideError.playFromStart();
-		hideSms.playFromStart();
-		hideCorrect.playFromStart();
-		hideIncorrect.playFromStart();
 		
+		hideOthers(-1);
 		colorNormal.playFromStart();
 	}
 
@@ -199,6 +89,55 @@ public class IsoPhone extends LayerIcon {
 
 		colorNormal = new Timeline(new KeyFrame(Duration.seconds(duration),
 				new KeyValue(getFillProperty(3), style.getAccent(), Interpolator.EASE_BOTH)));
-		colorNormal.setOnFinished(e-> mutex.put(0, false));
+	}
+
+	private class Transition {
+		Timeline show;
+		Timeline hide;
+		Timeline colorShow;
+
+		boolean showing = false;
+
+		Transition(int layer, double size, int by) {
+			setTranslateX(layer, by * size / 4.0);
+			setTranslateY(layer, by * size / (4.0 * factor));
+			setOpacity(layer, 0.0);
+			
+			show = new Timeline(new KeyFrame(Duration.seconds(duration),
+					new KeyValue(translateXProperty(layer), 0, SplineInterpolator.ANTICIPATEOVERSHOOT),
+					new KeyValue(translateYProperty(layer), 0, SplineInterpolator.ANTICIPATEOVERSHOOT),
+					new KeyValue(opacityProperty(layer), 1, SplineInterpolator.ANTICIPATEOVERSHOOT)));
+
+			show.setOnFinished(e -> showing = false);
+
+			hide = new Timeline(new KeyFrame(Duration.seconds(duration),
+					new KeyValue(translateXProperty(layer), by * size / 4.0, SplineInterpolator.ANTICIPATEOVERSHOOT),
+					new KeyValue(translateYProperty(layer), by * size / (4.0 * factor), SplineInterpolator.ANTICIPATEOVERSHOOT),
+					new KeyValue(opacityProperty(layer), 0, SplineInterpolator.ANTICIPATEOVERSHOOT)));
+
+			colorShow = new Timeline(new KeyFrame(Duration.seconds(duration),
+					new KeyValue(getFillProperty(3), by > 0 ? Colors.Error : Colors.GREEN, Interpolator.EASE_BOTH)));
+		}
+
+		void playShow() {
+			showing = true;
+			show.playFromStart();
+			colorShow.playFromStart();
+		}
+
+		void playHide() {
+			hide.playFromStart();
+		}
+		
+		public boolean isShowing() {
+			return showing;
+		}
+		
+		void stop() {
+			showing = false;
+			show.stop();
+			hide.stop();
+			colorShow.stop();
+		}
 	}
 }
